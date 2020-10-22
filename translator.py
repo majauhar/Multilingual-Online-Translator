9770fa9cac02bd30119a0b2236744b8ae252303e
Jauhar <siddmohamed19@gmail.com>
2020-10-22 14:44:54 +0530

    First stage done

--- translator.py
+++ translator.py
@@ -1,1 +1,5 @@
-print('Hello, World!')
+lang = input('Type "en" if you want to translate from French into English, or "fr" if you want to'
+             'translate from English into French\n')
+
+word = input('Type the word you want to translate:\n')
+print('You chose "{}" as the language to translate "{}" to.'.format(lang, word))
