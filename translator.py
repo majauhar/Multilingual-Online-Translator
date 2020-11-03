966085f4a07f3b552d9f6ffade6b2c863c449daa
Jauhar <siddmohamed19@gmail.com>
2020-11-03 19:03:47 +0530

    stage 2 in progress

--- translator.py
+++ translator.py
@@ -1,5 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+address = 'https://context.reverso.net/translation/english-french/cheese'
 lang = input('Type "en" if you want to translate from French into English, or "fr" if you want to'
              'translate from English into French\n')
 
 word = input('Type the word you want to translate:\n')
 print('You chose "{}" as the language to translate "{}" to.'.format(lang, word))
+if lang == 'fr':
+    add = address + 'english-french' + word
+    r = requests.get(add)
+    if r.status_code == 200:
+        print('200 OK')
+    soup = BeautifulSoup(r.content, 'html.parser')
